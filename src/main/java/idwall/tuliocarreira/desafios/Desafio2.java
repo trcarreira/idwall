5ca65349940c1e3579da8325b7b3dcfbef8fcc90
Tulio Carreira <trcarreira@gmail.com>
2017-01-09 06:08:07 -0200

    Comentando

--- src/main/java/idwall/tuliocarreira/desafios/Desafio2.java
+++ src/main/java/idwall/tuliocarreira/desafios/Desafio2.java
@@ -13,7 +13,8 @@ import com.github.jreddit.utils.restclient.RestClient;
 public class Desafio2 {
 	private static Scanner sc;
 
-	// lê o texto de input e agrupa as linhas em uma única String
+	// lê o texto de input e agrupa os subreddits delimitados por
+	// ponto-e-vírgula em uma lista de strings
 	public static List<String> leTextoEntrada() {
 		sc = new Scanner(System.in);
 		String[] input = sc.nextLine().split(";");
@@ -24,11 +25,15 @@ public class Desafio2 {
 		return subreddits;
 	}
 
+	// busca informações sobre um subreddit específico
 	public static List<Submission> buscaInfoSubreddit(Submissions subms, String subreddit) {
+		// busca as 100 threads mais "populares"
 		List<Submission> threads = subms.ofSubreddit(subreddit, SubmissionSort.TOP, -1, 100, null, null, true);
 		List<Submission> threadsSelecionadas = new ArrayList<Submission>();
 		for (Submission s : threads) {
 			if (s.getScore() >= 500) {
+				// quando o score de uma thread é maior que 500, ela é
+				// adicionada à lista de retorno
 				threadsSelecionadas.add(s);
 			}
 		}
@@ -37,19 +42,19 @@ public class Desafio2 {
 
 	public static void main(String[] args) {
 		List<String> subreddits = leTextoEntrada();
-		// Initialize REST Client
 
+		// Inicializa o REST Client
 		RestClient restClient = new HttpRestClient();
 		restClient.setUserAgent("bot/1.0 by name");
 
-		// Handle to Submissions, which offers the basic API submission
-		// functionality
+		// Manipula a API de Submissions
 		Submissions subms = new Submissions(restClient);
 		List<Submission> threadsEscolhidas = new ArrayList<Submission>();
 
 		for (String subreddit : subreddits) {
 			threadsEscolhidas = buscaInfoSubreddit(subms, subreddit);
 			for (Submission thread : threadsEscolhidas) {
+				// Exibe algumas informações para cada thread selecionada
 				System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 				System.out.println("Subreddit: " + thread.getSubreddit());
 				System.out.println("Título: " + thread.getTitle());
